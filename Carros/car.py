dbcfd6592b6882dab56fe425ddb3cf1e3057541a
Eric Gomes Gregory <49287457+EricGomesGregory@users.noreply.github.com>
2022-08-17 10:32:58 -0300

    Created car class and updated main

---
+++ Carros/car.py
@@ -0,0 +1,40 @@
+import pygame
+
+class Car (object):
+    def __init__(self, x, y, img) -> None:
+        self.x = x
+        self.y = y
+        self.pos = (x, y)
+        self.image = img
+        self.fliped = False
+        self.flip_cout = 0
+
+    def update(self, screen : pygame.Surface, speed: int, step: int):
+        if self.flip_cout == 3:
+            return
+
+        if not self.fliped:
+            if self.x < screen.get_width():
+                self.x += speed
+            else:
+                self.y += step
+                self.fliped = True
+                self.flip_cout += 1
+        
+        if self.fliped:
+            if self.x > 0 - self.image.get_width():
+                self.x -= speed
+            else:
+                self.y += step
+                self.fliped = False
+                self.flip_cout += 1
+
+        self.pos = (self.x, self.y)
+        print("Car: {} {}".format(self.pos, self.fliped))
+
+    def draw(self, screen : pygame.Surface):
+        if self.fliped:
+            fliped_img = pygame.transform.flip(self.image, True, False)
+            screen.blit(fliped_img, self.pos)
+        else:
+            screen.blit(self.image, self.pos)
--- Carros/main.py
+++ Carros/main.py
@@ -1,3 +1,4 @@
+from car import Car
 import pygame
 pygame.init()
 
@@ -8,17 +9,24 @@ screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Car Game")
 
 background = pygame.image.load("Carros\\assets\screen.png")
-car = pygame.image.load("Carros\\assets\\ferrari1.png")
+car_img = pygame.image.load("Carros\\assets\\ferrari1.png")
+
+car = Car(50, 50, car_img)
 
 playing = True
 
 while playing:
     screen.blit(background, (0,0))
-    screen.blit(car, (int(width/2), int(height/2)))
+    car.draw(screen)
+    car.update(screen, 1, 120)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             playing = False
+
+    if car.flip_cout == 3:
+        pygame.time.delay(1000)
+        playing = False
 
     pygame.display.update()
 
